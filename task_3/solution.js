285cca967acb2f12df7166d8ea4e53d21374a178
Ilya Sergeev <sergeev_ilya30@mail.ru>
2023-02-03 23:51:15 +0300

    task 3 done

--- task_3/solution.js
+++ task_3/solution.js
@@ -3,13 +3,20 @@ function renderCartItem(item) {
 
     // Задание №3.1. Формирование строки корзины
 
-    // product - объект вида {id: id, title: title, count: count, price: price}
+   // product - объект вида {id: id, title: title, count: count, price: price}
     // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
     // id - идентификатор
     // title - наименование
     // price - цена
     // count - количество
-
+    let itemCountText = "";
+    itemCountText += product.count;
+    itemCountText += ` × `;
+    itemCountText += product.price;
+    itemCountText += ` ₽ = `;
+    let sum = product.count * product.price;
+    itemCountText += sum;
+    itemCountText += ` ₽`;
     // сформируйте строку itemCountText вида:
     // количество × цена ₽ = стоимость ₽
     // например,
